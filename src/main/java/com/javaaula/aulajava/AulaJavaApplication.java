4baf09fdd90f9b1807d4e04a7dc6f49a5bef513a
Pedromontesi <joaopedromonteiro090@hotmail.com>
2023-10-09 11:53:13 -0300

    Project Created

---
+++ src/main/java/com/javaaula/aulajava/AulaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.javaaula.aulajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaaula/aulajava/AulaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaaula.aulajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

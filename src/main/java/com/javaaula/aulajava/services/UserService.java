20439491da27cf2d8cb9568287d7fd82dab061a9
Pedromontesi <joaopedromonteiro090@hotmail.com>
2023-10-23 17:55:11 -0300

    User Update

--- src/main/java/com/javaaula/aulajava/services/UserService.java
+++ src/main/java/com/javaaula/aulajava/services/UserService.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.javaaula.aulajava.entities.User;
 import com.javaaula.aulajava.repositories.UserRepository;
@@ -27,11 +31,21 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
 
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
-	
